2537d8a5703fec26c474eff2805bc5db02afada1
wangjinyu <wangjinyu@gooalgene.com>
2019-10-10 15:30:31 +0800

    sx

--- src/main/java/com/wjy/util/IpUtils.java
+++
@@ -1,9 +0,0 @@
-package com.wjy.util;
-
-import com.ggstar.util.ip.IpHelper;
-
-public class IpUtils {
-    public static String getCity(String ip){
-        return IpHelper.findRegionByIp(ip);
-    }
-}
